2145805128e417237d601a879007ab1474bbd480
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2023-10-16 21:42:26 +0600

    create sign in api route

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -1,4 +1,15 @@
+/*
+ * Title: User Auth Controller 
+ * Description: Handle User Authentication
+ * Author: Md Abdullah
+ * Date: 10/16/23
+ */
+
+
+
+//Dependencies:
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -14,5 +25,39 @@ export const signup = async(req, res, next) => {
         res.status(201).json({message: 'user created successfully'})
     } catch (error) {
         next(errorHandler(550, 'Could not create new user.'));
+    }
+}
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+   
+    try {
+        const validUser = await User.findOne({ email });
+
+        if (!validUser) {
+            return errorHandler(404, 'Wrong credentials!');
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) return errorHandler(401, 'Wrong credentials!');
+        
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        const expirationDate = new Date();
+        expirationDate.setTime(expirationDate.getTime() + 24 * 60 * 60 * 1000);
+
+
+        const { password: pass, ...rest } = validUser._doc;
+
+        res.cookie('access_token', token, { httpOnly: true, expires: expirationDate })
+        .status(200)
+        .json(rest);
+
+    } catch (error) {
+        next(errorHandler(400, 'Could not find user.'));
     }
 }
