2a03127fa00d4b239778adbc8c00a587309878b2
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2023-10-25 21:05:34 +0600

    create user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,7 +8,7 @@
 
 //Dependencies:
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUserListing, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const userRouter = express.Router();
@@ -16,6 +16,7 @@ const userRouter = express.Router();
 
 //Routes:
 userRouter.get('/test', test);
+userRouter.get('/listings/:id',verifyToken, getUserListing);
 userRouter.post('/update/:id', verifyToken, updateUser);
 userRouter.delete('/delete/:id', verifyToken, deleteUser);
 export default userRouter;
