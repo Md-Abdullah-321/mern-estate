f914c4bf68b627317363e9a5c1bcd92e4249406f
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2023-10-27 23:04:53 +0600

    create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Listing from "./pages/Listing";
 import Profile from "./pages/Profile";
+import Search from "./pages/Search";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import CreateListing from "./pages/createListing";
@@ -19,6 +20,7 @@ function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing />} />
 
         <Route element={<PrivateRoute />}>
