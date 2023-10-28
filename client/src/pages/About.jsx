6deaa98d05dfda94335c3a3300afae2a201db09a
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2023-10-28 20:19:02 +0600

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,38 @@
 function About() {
-  return <div className="bg-gray-100">About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Real Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        At Real Estate website, we're your dedicated partner in finding your
+        dream property or selling your valuable asset. Our journey started with
+        a passion for connecting people with homes and investment opportunities.
+        We founded Real Estate to create a user-friendly and efficient real
+        estate experience, blending technology and personal touch.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Our mission is clear: empower you with tools and knowledge for informed
+        real estate decisions. We understand the significance of property
+        transactions and are committed to guiding you from search to handshake.
+        Our team of experts works to give you market insights, property
+        listings, and innovative tools, ensuring you can achieve your
+        homeownership dreams.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        What sets us apart is our commitment to transparency, integrity, and
+        excellent customer service. Trust is the foundation of real estate, and
+        we take it seriously. We promise accurate, up-to-date information to
+        help you make confident decisions. Whether you're a first-time
+        homebuyer, investor, or seller, Real Estate simplifies your real estate
+        journey. Explore our platform, experience our dedication, and let us be
+        your partner in realizing your real estate dreams. Thank you for
+        choosing Real Estate Website as your trusted real estate resource.
+      </p>
+    </div>
+  );
 }
 
 export default About;
